1108cb8946af7606d19c51d8df99276b4e1eda50
Mark <markgatus@hotmail.com>
2022-07-15 14:00:29 +1000

    Created classes for position, row and positionMap added some CSS

--- app.ts
+++ app.ts
@@ -1,1 +1,58 @@
-console.log("Hello world2");
+enum STATUS {
+    AVAILABLE = 'AVAILABLE',
+    OCCUPIEDP1 = 'OCCUPIEDP1',
+    OCCUPIEDP2 = 'OCCUPIEDP2',
+    SELECTED = 'SELECTED'
+}
+
+class Position {
+    id: number
+    status: STATUS
+    element: HTMLDivElement
+
+    constructor(id: number, isOccupiedP1: boolean = false, isOccupiedP2: boolean = false) {
+        this.id = id
+        this.status = isOccupiedP1 ? STATUS.OCCUPIEDP1: STATUS.AVAILABLE // need to add OCCUPIEDP2
+        // Need to check status somehow.
+        this.element = document.createElement('div')
+        this.element.classList.add('position')
+        this.element.classList.add(this.status.toLowerCase())
+    }   
+}
+
+class Row {
+    id: number
+    positions: Position[]
+    element: HTMLDivElement
+
+    constructor(id: number, positionNumber: number) {
+        this.id = id
+        this.positions = Array.from({length: positionNumber}).map((_, index) => {
+            const positionId = positionNumber * id + index
+            return new Position(positionId)
+        })
+        this.element = document.createElement('div')
+        this.element.classList.add('row')
+        this.element.append(...this.positions.map((position) => position.element))
+    }
+}
+
+class PositionMap { // See L10 8:30
+    rows: Row[]
+    selectedSeats: number[] = []
+    element: HTMLDivElement
+
+    constructor(rowNumber: number, positionNumberPerRow: number, occupiedPositions: number[] = []){
+        this.rows = Array.from({length: rowNumber}).map((_, index) => {
+            return new Row(index, positionNumberPerRow)
+        })
+        this.element = document.createElement('div')
+        this.element.classList.add('position-map')
+        this.element.append(...this.rows.map((row) => row.element))
+    }
+}
+
+const positionMap = new PositionMap(5, 10);
+document.getElementById('gameboard')?.appendChild(positionMap.element)
+
+
